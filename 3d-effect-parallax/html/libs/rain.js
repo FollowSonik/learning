576ab8db6480c3792e49a372b2e5f8861a940913
FollowSonik <sonic_906@mail.ru>
2023-07-24 17:02:06 +0700

    3d-effects-html-css is completed.

---
+++ 3d-effect-parallax/html/libs/rain.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementsByClassName("rain")[0];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext("2d");
+
+function randomNum(max, min) {
+	return Math.floor(Math.random() * max) + min;
+}
+
+function RainDrops(x, y, endy, velocity, opacity) {
+	this.x = x;
+	this.y = y;
+	this.endy = endy;
+	this.velocity = velocity;
+	this.opacity = opacity;
+
+	this.draw = function() {
+		c.beginPath();
+		c.moveTo(this.x, this.y);
+		c.lineTo(this.x, this.y - this.endy);
+		c.lineWidth = 1;
+		c.strokeStyle= `rgba(255, 255, 255, ${this.opacity})`;
+		c.stroke();
+	}
+
+	this.update = function() {
+		const rainEnd = window.innerHeight + 100;
+		if (this.y >= rainEnd) {
+			this.y = this.endy - 100;
+		} else {
+			this.y = this.y + this.velocity;
+		}
+		this.draw();
+	}
+}
+
+const rainArray = [];
+
+for (let i = 0; i < 240; i++) {
+	const rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
+	const rainYLocation = Math.random() * -500;
+	const randomRainHeight = randomNum(3, .5);
+	const randomSpeed = randomNum(20, .4);
+	const randomOpacity = Math.random() * .55;
+
+	rainArray.push(new RainDrops(rainXLocation, rainYLocation, randomRainHeight, randomSpeed, randomOpacity));
+}
+
+function animateRain() {
+	requestAnimationFrame(animateRain);
+	c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+	for (let i = 0; i < rainArray.length; i++) {
+		rainArray[i].update();
+	}
+}
+
+animateRain();
